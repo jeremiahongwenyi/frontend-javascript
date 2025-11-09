6770a611c75dd2f22ce4c03ce4118e3c89fb69d5
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-11-09 23:50:34 +0300

    question 2 and 3

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,41 @@
+interface Teacher {
+  readonly firstName: string;        // cannot be changed after initialization
+  readonly lastName: string;         // cannot be changed after initialization
+  fullTimeEmployee: boolean;         // must always exist
+  yearsOfExperience?: number;        // optional
+  location: string;                  // must always exist
+  [key: string]: any;                // allows adding extra attributes like contract
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
